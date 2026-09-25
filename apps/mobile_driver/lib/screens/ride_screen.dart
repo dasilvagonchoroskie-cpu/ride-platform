1336bc54dbaf0afc7cd664f51ba10e14d2108666
dasilvagonchoroskie-cpu <dasilvagonchoroskie@gmail.com>
2026-09-24 21:49:34 -0300

    conserta os 13 imports que sobraram do rename uber_theme -> app_theme

--- apps/mobile_driver/lib/screens/ride_screen.dart
+++ apps/mobile_driver/lib/screens/ride_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../core/theme/uber_theme.dart';
+import '../core/theme/app_theme.dart';
 import '../core/utils/formatters.dart';
 import '../core/utils/geo.dart';
 import '../data/models/driver_models.dart';
