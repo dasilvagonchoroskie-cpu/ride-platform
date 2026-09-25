9154f3bef0d44514d3b6e8a1b9be94a84e60e722
dasilvagonchoroskie-cpu <dasilvagonchoroskie@gmail.com>
2026-09-24 21:49:38 -0300

    conserta os 13 imports que sobraram do rename uber_theme -> app_theme

--- apps/mobile_driver/lib/screens/splash_screen.dart
+++ apps/mobile_driver/lib/screens/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../core/theme/uber_theme.dart';
+import '../core/theme/app_theme.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
