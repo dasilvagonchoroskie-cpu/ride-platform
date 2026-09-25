d55b6fd17e1f551a803259fba0c6c66f2ba22eb1
dasilvagonchoroskie-cpu <dasilvagonchoroskie@gmail.com>
2026-09-24 21:49:40 -0300

    conserta os 13 imports que sobraram do rename uber_theme -> app_theme

--- apps/mobile_driver/lib/widgets/ride_map.dart
+++ apps/mobile_driver/lib/widgets/ride_map.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
-import '../core/theme/uber_theme.dart';
+import '../core/theme/app_theme.dart';
 import '../core/utils/geo.dart';
 
 enum MarkerKind { pickup, dropoff, driver, car }
